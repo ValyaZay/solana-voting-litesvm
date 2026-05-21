10536f56c64a5ea42a6a625af871e36f0bd26f63
Valya Zaitseva <val.zaitseva@gmail.com>
2026-05-21 09:53:27 +0000

    add vote

--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -1,12 +1,15 @@
 use anchor_lang::prelude::*;
 
-declare_id!("9UKZFA4opT5zYuq5SidXdeLLMpxGMtGMybYMJfL2LyKZ");
+pub use error::Errors;
+pub mod error;
+
+declare_id!("3b9t3AxeTPFqoNCAaWcocGCwADe4nm2cJ4u1TPXWVf6a");
 
 #[program]
 pub mod voting {
     use super::*;
 
-    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, start: u64, end: u64, name: String, description: String,) -> Result<()> {
+    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, start: i64, end: i64, name: String, description: String,) -> Result<()> {
         let poll = &mut ctx.accounts.poll_account;
         poll.poll_name = name;
         poll.poll_description = description;
@@ -19,6 +22,16 @@ pub mod voting {
     pub fn init_candidate(ctx: Context<InitCandidate>, _poll_id: u64, candidate_name: String,) -> Result<()> {
         ctx.accounts.candidate_account.candidate_name = candidate_name;
         ctx.accounts.poll_account.poll_candidates_amount += 1;
+
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, _poll_id: u64, _candidate_name: String) -> Result<()> {
+        let poll_account = &ctx.accounts.poll_account;
+        let now = Clock::get()?.unix_timestamp;
+        require!(now > poll_account.poll_voting_start && now < poll_account.poll_voting_end, Errors::PollNotActive);
+        
+        ctx.accounts.candidate_account.candidate_votes += 1;
 
         Ok(())
     }
@@ -40,6 +53,26 @@ pub struct InitPoll<'info> {
     pub poll_account: Account<'info, PollAccount>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate_name: String)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_ref()],
+        bump,
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
+
+    #[account(
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll_account: Account<'info, PollAccount>,
 }
 
 #[derive(Accounts)]
@@ -74,8 +107,8 @@ pub struct PollAccount {
     pub poll_name: String,
     #[max_len(280)]
     pub poll_description: String,
-    pub poll_voting_start: u64,
-    pub poll_voting_end: u64,
+    pub poll_voting_start: i64,
+    pub poll_voting_end: i64,
     pub poll_candidates_amount: u64,
 }
 
